e6176ebdf023955ef9a345ac1437279a3eaf1a56
EdgeTypE <minefpsgelistirici@gmail.com>
2026-06-22 11:48:50 +0300

    Bump version to 0.1.10
    I couldn't decide whether the new version should be 0.2.0 or 0.1.10. It was simultaneously 0.1.10 and 0.2.0 until I pushed this commit.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,9 +15,9 @@ android {
         applicationId = "com.betterdeepseek.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         // Keep in sync with package.json "version" and static/manifest.json "version".
-        versionName = "0.1.9"
+        versionName = "0.1.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
